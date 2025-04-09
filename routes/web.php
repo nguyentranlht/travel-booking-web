d5c5ae42f1179acc77c7bc95fc25968a560cdd01
Nguyen Tran <nguyentranlht@gmail.com>
2025-04-09 10:06:42 +0700

    SCRUM-71 <message>

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'role:user,tour_rider'])->name('dashboard');
